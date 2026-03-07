c07821e005974ed5a4ea3fb312e14f008dfd415a
Matheus Farias <matheussfrss@gmail.com>
2026-03-06 23:50:47 -0300

    portabackecorfront

--- backend/app.py
+++ backend/app.py
@@ -251,8 +251,9 @@ def home():
 
 if __name__ == "__main__":
 
+    port = int(os.environ.get("PORT", 10000))
+
     app.run(
         host="0.0.0.0",
-        port=3080,
-        debug=True
+        port=port
     )
